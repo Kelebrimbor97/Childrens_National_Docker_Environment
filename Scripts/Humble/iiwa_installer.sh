8e0f30aecb9a6f341d6d377feb0a38709beb8d00
nishadkulkarni <nishadkulkarni97@gmail.com>
2024-02-14 11:03:59 -0500

    Massive overhaul - now ROS image is base and used separately for projects with separate images

--- Scripts/Humble/iiwa_installer.sh
+++
@@ -1,9 +0,0 @@
-mkdir -p ~/lbr-stack/src && cd ~/lbr-stack
-wget https://raw.githubusercontent.com/lbr-stack/lbr_fri_ros2_stack/humble/lbr_fri_ros2_stack/repos.yaml -P src
-vcs import src < src/repos.yaml
-source opt/ros/humble/setup.bash
-rosdep install --from-paths src --ignore-src -r -y
-source /opt/ros/humble/setup.bash
-echo "source /opt/ros/humble/setup.bash">>~/.bashrc
-# colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release --symlink-install
-exec "$@"
