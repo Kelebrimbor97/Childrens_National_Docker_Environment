c6119cc0fe5c26c8f90bcbb039adf239b36343b4
nishadkulkarni <nishadkulkarni97@gmail.com>
2024-03-20 15:18:42 -0400

    docker updates

--- Scripts/Humble/cpu/KUKA/iiwa_installer.sh
+++ Scripts/Humble/cpu/KUKA/iiwa_installer.sh
@@ -2,6 +2,7 @@ mkdir -p ~/lbr-stack/src && cd ~/lbr-stack
 wget https://raw.githubusercontent.com/lbr-stack/lbr_fri_ros2_stack/humble/lbr_fri_ros2_stack/repos.yaml -P src
 vcs import src < src/repos.yaml
 source opt/ros/humble/setup.bash
+rosdep update
 rosdep install --from-paths src --ignore-src -r -y
 source /opt/ros/humble/setup.bash
 echo "source /opt/ros/humble/setup.bash">>~/.bashrc
--- Scripts/Humble/cuda/KUKA/iiwa_installer.sh
+++ Scripts/Humble/cuda/KUKA/iiwa_installer.sh
@@ -1,7 +1,8 @@
 mkdir -p ~/lbr-stack/src && cd ~/lbr-stack
 wget https://raw.githubusercontent.com/lbr-stack/lbr_fri_ros2_stack/humble/lbr_fri_ros2_stack/repos.yaml -P src
 vcs import src < src/repos.yaml
-source opt/ros/humble/setup.bash
+source /opt/ros/humble/setup.bash
+rosdep update
 rosdep install --from-paths src --ignore-src -r -y
 source /opt/ros/humble/setup.bash
 echo "source /opt/ros/humble/setup.bash">>~/.bashrc
